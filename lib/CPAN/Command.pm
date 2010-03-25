5fc48d855607a3d57bc1cec3805c341b4e21e44e
Marcel Grünauer <marcel@cpan.org>
2010-03-25 19:52:04 +0000

    Import of MARCEL/CPAN-Command-1.100840 from CPAN.
    gitpan-cpan-distribution: CPAN-Command
    gitpan-cpan-version:      1.100840
    gitpan-cpan-path:         MARCEL/CPAN-Command-1.100840.tar.gz
    gitpan-cpan-author:       MARCEL
    gitpan-cpan-maturity:     released

--- lib/CPAN/Command.pm
+++ lib/CPAN/Command.pm
@@ -1,22 +1,28 @@
-package CPAN::Command;
-
+use 5.008_001;
 use strict;
 use warnings;
-use 5.008_001;
+
+package CPAN::Command;
+our $VERSION = '1.100840';
+# ABSTRACT: Load CPAN command plugins
 use CPAN;    # so the plugins have something to work with
 use Module::Pluggable
-    search_path => [ 'CPAN::Command' ],
-    require     => 1;
-our $VERSION = '0.01';
-BEGIN { __PACKAGE__->plugins }  # just load them
-
+  search_path => ['CPAN::Command'],
+  require     => 1;
+BEGIN { __PACKAGE__->plugins }    # just load them
 1;
 
+
 __END__
+=pod
 
 =head1 NAME
 
-CPAN::Command - Load CPAN::Command::* plugins
+CPAN::Command - Load CPAN command plugins
+
+=head1 VERSION
+
+version 1.100840
 
 =head1 SYNOPSIS
 
@@ -39,38 +45,39 @@ command see L<CPAN::Command::Bugtracker>.
 This module also loads the L<CPAN> package so you can run, for example, its
 C<shell()> function.
 
+=head1 INSTALLATION
+
+See perlmodinstall for information and options on installing Perl modules.
+
 =head1 BUGS AND LIMITATIONS
 
 No bugs have been reported.
 
 Please report any bugs or feature requests through the web interface at
-L<http://rt.cpan.org>.
-
-=head1 INSTALLATION
-
-See perlmodinstall for information and options on installing Perl modules.
+L<http://rt.cpan.org/Public/Dist/Display.html?Name=CPAN-Command>.
 
 =head1 AVAILABILITY
 
 The latest version of this module is available from the Comprehensive Perl
 Archive Network (CPAN). Visit L<http://www.perl.com/CPAN/> to find a CPAN
-site near you. Or see L<http://www.perl.com/CPAN/authors/id/M/MA/MARCEL/>.
+site near you, or see
+L<http://search.cpan.org/dist/CPAN-Command/>.
 
-The development version lives at L<http://github.com/hanekomu/cpan-commander/>.
+The development version lives at
+L<http://github.com/hanekomu/CPAN-Command/>.
 Instead of sending patches, please fork this project using the standard git
 and github infrastructure.
 
-=head1 AUTHORS
+=head1 AUTHOR
 
-Marcel GrE<uuml>nauer, C<< <marcel@cpan.org> >>
-
-This distribution was inspired by L<CPAN::Command::Repo> by Tatsuhiko Miyagawa.
+  Marcel Gruenauer <marcel@cpan.org>
 
 =head1 COPYRIGHT AND LICENSE
 
-Copyright 2009 by Marcel GrE<uuml>nauer
+This software is copyright (c) 2009 by Marcel Gruenauer.
 
-This library is free software; you can redistribute it and/or modify
-it under the same terms as Perl itself.
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
 
 =cut
+
